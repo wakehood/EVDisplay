024670ccd54de20cf1215a30b2159f4dcdafcb8d
wakehood <wakehood@sonic.net>
2026-07-15 10:13:35 -0700

    Initial Commit

---
+++ EvDisplay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EvDisplay
+//
+//  Created by Sylvia Wake-Hood on 7/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EvDisplay/EvDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvDisplayApp.swift
+//  EvDisplay
+//
+//  Created by Sylvia Wake-Hood on 7/15/26.
+//
+
+import SwiftUI
+
+@main
+struct EvDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
